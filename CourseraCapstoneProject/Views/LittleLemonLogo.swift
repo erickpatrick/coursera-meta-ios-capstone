25426dbfcbf7643ccc0d34ce6b84795182848b16
erick patrick <github@erickpatrick.net>
2025-01-07 23:19:00 +0100

    adds custom logo and custom fonts

---
+++ CourseraCapstoneProject/Views/LittleLemonLogo.swift
@@ -0,0 +1,20 @@
+//
+//  LittleLemonLogo.swift
+//  CourseraCapstoneProject
+//
+//  Created by Erick Rocha on 07.01.25.
+//
+
+import SwiftUI
+
+struct LittleLemonLogo: View {
+    var body: some View {
+        Image("littleLemon")
+            .resizable()
+            .scaledToFit()
+    }
+}
+
+#Preview {
+    LittleLemonLogo()
+}
